24a98574e82a9bb07dcade9474d04b6fc4b6eabb
extraErik <erygg01@live.com>
2017-01-31 16:33:19 -0600

    Adjust FE validation highlighting logic. Some jQuery selector refactoring.

--- public/js/index.js
+++ public/js/index.js
@@ -1,57 +1,65 @@
 (function(){
 
     var socket = io();
+
+    var joinForm = jQuery("#join-form");
+    var nameInputField = jQuery('.name-input-field');
+    var nameTextInput = jQuery('#name');
+    var roomInputFields = jQuery('.room-input-field');
     var existingRooms = jQuery('#existing-rooms');
+    var roomTextInput = jQuery("#room");
 
     socket.on('updateActiveRoomsList', function (rooms) {
 
         existingRooms.empty();
-
-        var roomSelectInput = jQuery('#existing-rooms');
 
         if (rooms.length > 0) {
             existingRooms.append(jQuery('<option value=""></option>').text("Select a room"));
             rooms.forEach(function (roomName) {
                 existingRooms.append(jQuery('<option value="' + roomName + '"></option>').text(roomName));
             });
-            roomSelectInput.prop('disabled', false);
+            existingRooms.prop('disabled', false);
         } else {
             existingRooms.append(jQuery('<option value=""></option>').text("No active rooms"));
-            roomSelectInput.prop('disabled', true);
+            existingRooms.prop('disabled', true);
         }
     });
 
     function clearRoomSelect(flag) {
         if (flag) {
-            jQuery('#existing-rooms').val('');
-            jQuery('#existing-rooms').closest('.form-field').addClass('gray-out');
+            existingRooms.val('');
+            existingRooms.closest('.form-field').addClass('gray-out');
         } else {
-            jQuery('#existing-rooms').closest('.form-field').removeClass('gray-out');
+            existingRooms.closest('.form-field').removeClass('gray-out');
         }
     };
 
     function clearRoomInput(flag) {
         if (flag) {
-            jQuery('#room').val('');
-            jQuery('#room').closest('.form-field').addClass('gray-out');
+            roomTextInput.val('');
+            roomTextInput.closest('.form-field').addClass('gray-out');
         } else {
-            jQuery('#room').closest('.form-field').removeClass('gray-out');
+            roomTextInput.closest('.form-field').removeClass('gray-out');
         }
     };
 
-    var roomSelectInput = jQuery("#existing-rooms");
-    roomSelectInput.on('focus', function(event) {
+    nameTextInput.on('keyup', function(event) {
+        nameInputField.removeClass('error');
+    });
+
+    existingRooms.on('focus', function(event) {
         clearRoomSelect(false);
     });
-    roomSelectInput.on('change', function(event) {
+    existingRooms.on('change', function(event) {
+        roomInputFields.removeClass('error');
         clearRoomInput(true);
     });
 
-    var roomTextInput = jQuery("#room");
     roomTextInput.on('focus', function(event) {
         clearRoomInput(false);
     });
     roomTextInput.on('keyup', function(event) {
+        roomInputFields.removeClass('error');
         var roomTextInputVal = roomTextInput.val().trim();
         if (roomTextInputVal.length > 0) {
             clearRoomSelect(true);
@@ -67,11 +75,10 @@
         }
     });
 
-    var joinForm = jQuery("#join-form");
     joinForm.submit(function(event) {
         event.preventDefault();
 
-        var name = joinForm.find('#name').val().trim();
+        var name = nameTextInput.val().trim();
         var room;
 
         var selectedRoom = joinForm.find('.room-input-field select').val().trim();
@@ -80,19 +87,16 @@
         room = newRoom ? newRoom : selectedRoom;
         room = _.startCase(room);
 
-        var nameInputField = jQuery('.name-input-field');
-        var roomInputFields = jQuery('.room-input-field');
-
         nameInputField.removeClass('error');
         roomInputFields.removeClass('error');
         if (!name || !room) {
             if (!room) {
                 roomInputFields.addClass('error');
-                joinForm.find('.room-input-field input').focus();
+                roomTextInput.focus();
             }
             if (!name) {
                 nameInputField.addClass('error');
-                joinForm.find('#name').focus();
+                nameTextInput.focus();
             }
         } else {
             var newUrl = joinForm.attr('action') + '?name=' + name + '&room=' + room;
